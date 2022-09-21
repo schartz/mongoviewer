824450e61c47cba05488dfca7ec1ded929ade3e2
Schartz Rehan <rehan.qt@gmail.com>
2022-09-21 16:16:55 +0530

    add a dummy menu

--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"context"
 	"embed"
+	"github.com/wailsapp/wails/v2/pkg/menu"
+	"github.com/wailsapp/wails/v2/pkg/menu/keys"
+	"github.com/wailsapp/wails/v2/pkg/runtime"
 
 	"github.com/wailsapp/wails/v2"
 	"github.com/wailsapp/wails/v2/pkg/options"
@@ -11,6 +15,25 @@ import (
 var assets embed.FS
 
 func main() {
+	AppMenu := menu.NewMenu()
+	FileMenu := AppMenu.AddSubmenu("File")
+	FileMenu.AddText("Open", keys.CmdOrCtrl("o"), func(_ *menu.CallbackData) {
+		println("open file")
+	})
+	FileMenu.AddSeparator()
+	FileMenu.AddText("Quit", keys.CmdOrCtrl("q"), func(_ *menu.CallbackData) {
+		runtime.Quit(context.Background())
+	})
+
+	HelpMenu := AppMenu.AddSubmenu("Help")
+	HelpMenu.AddText("About", nil, func(_ *menu.CallbackData) {
+		println("About App")
+	})
+
+	//if runtime.GOOS == "darwin" {
+	//	AppMenu.Append(menu.EditMenu()) // on macos platform, we should append EditMenu to enable Cmd+C,Cmd+V,Cmd+Z... shortcut
+	//}
+
 	// Create an instance of the app structure
 	app := NewApp()
 
@@ -22,6 +45,7 @@ func main() {
 		Assets:           assets,
 		BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 1},
 		OnStartup:        app.startup,
+		Menu:             AppMenu,
 		Bind: []interface{}{
 			app,
 		},
